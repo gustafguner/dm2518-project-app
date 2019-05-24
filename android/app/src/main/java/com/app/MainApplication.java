6d5b5f98dce414759349de850b1f7682ffd00dc2
Gustaf Gunér <gustaf@guner.se>
2019-05-24 12:15:10 +0200

    SVG + tab bar icons

--- android/app/src/main/java/com/app/MainApplication.java
+++ android/app/src/main/java/com/app/MainApplication.java
@@ -3,6 +3,7 @@ package com.app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.RNRSA.RNRSAPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new AsyncStoragePackage(),
             new RNRSAPackage(),
             new RNGestureHandlerPackage()
--- src/screens/profile/index.tsx
+++ src/screens/profile/index.tsx
@@ -11,25 +11,38 @@ import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
 const PROFILE_QUERY = gql`
-  query User($input: FindUserInput!) {
-    user(input: $input) {
+  query User($username: String) {
+    user(username: $username) {
+      username
       publicKey
     }
   }
 `;
 
+interface User {
+  username: string;
+  publicKey: string;
+}
+
+interface Response {
+  user: User;
+}
+
+interface Variables {
+  username?: string;
+}
+
 const ProfileScreen: NavigationScreenComponent<NavigationScreenProps> = () => {
   return (
     <View>
-      <Text>Profile</Text>
-      <Query
-        query={PROFILE_QUERY}
-        variables={{ input: { username: 'gustaf' } }}
-      >
-        {({ data, loading, err }) => {
-          console.log(data);
-          return null;
-        }}
+      <Query<Response, Variables> query={PROFILE_QUERY}>
+        {({ data, loading, error }) =>
+          data && !loading && !error ? (
+            <Text>{data.user.username}</Text>
+          ) : (
+            <Text>Loading...</Text>
+          )
+        }
       </Query>
     </View>
   );
